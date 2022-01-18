28c11914abe00829f800f5267cbd0c62e1d07507
Igor Denissov <igordenissov83@gmail.com>
2022-01-18 16:15:36 +0300

    added css module

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import classes from './Navbar.module.css';
 
 const Navbar = () => {
   return (
-      <nav className='nav'>
-          <div>
+      <nav className={classes.nav}>
+          <div className = {classes.item}>
               <a href="">Profile</a>
           </div>
-          <div>
+          <div className = "item">
               <a href="">Messages</a>
           </div>
-          <div>
+          <div className = "item">
               <a href="">News</a>
           </div>
-          <div>
+          <div className = "item">
               <a href="">Music</a>
           </div>
-          <div>
+          <div className = "item">
               <a href="">Settings</a>
           </div>
       </nav>
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
   return (
-      <div className='content'>
+      <div className={classes.content}>
           <div>
               <img src="" alt=""/>
           </div>
