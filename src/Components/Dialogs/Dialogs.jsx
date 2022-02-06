b365add0359e8752f4a3badfc37f07acfe477da8
Igor Denissov <igordenissov83@gmail.com>
2022-02-07 02:15:09 +0300

    divided dialogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 const Dialogs = (props) => {
   return (
-      <div>
-          Message filed
+      <div className={classes.dialogs}>
+          <div className={classes.dialogsItems}>
+              <div className={classes.dialog}>
+                  Bob
+              </div>
+              <div className={classes.dialog}>
+                  Anna
+              </div>
+              <div className={classes.dialog}>
+                  Jonny
+              </div>
+              <div className={classes.dialog}>
+                  Ron
+              </div>
+              <div className={classes.dialog}>
+                  Helen
+              </div>
+              <div className={classes.dialog}>
+                  Hope
+              </div>
+          </div>
+          <div className={classes.messages}>
+              <div className={classes.message}>Hi</div>
+              <div className={classes.message}>How</div>
+              <div className={classes.message}>Great</div>
+          </div>
       </div>
   )
 }
